1ad16b6e8087abdefb157aba7f69224993e116af
raymond-le-goldenowl <raymond.le.goldenowl@gmail.com>
2022-04-22 15:41:21 +0700

    refator app

---
+++ src/containers/App/index.jsx
@@ -0,0 +1,6 @@
+import { APP_NAME } from './constants';
+function App() {
+	return <div className='app-vuleshopbee'>{APP_NAME}</div>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root');
